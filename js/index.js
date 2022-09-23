493f9743e064ef2d8a5d29d4240dae25bbf2e2b7
Litvinov Iurii <litvinov.yuriy1996@gmail.com>
2022-09-23 19:54:25 +0400

    Added pages

--- js/index.js
+++ js/index.js
@@ -1,44 +1,59 @@
-$date = $('.calc-summary__item--date .calc-summary__value');
-$summ = $('.calc-summary__item--summ .calc-summary__value');
-$('.range-slider').each(function() {
-  var min = Number($(this).attr('data-min')),
-      max = Number($(this).attr('data-max')),
-      value = Number($(this).attr('data-value')),
-      step = Number($(this).attr('data-step')),
-      separator = $(this).attr('data-separator'),
-      $this = $(this);
-      if (separator !== '₽') setCalcDate(value);
-  
-  $this.slider({
-    range: 'min',
-    value,
-    min,
-    max,
-    step,
-    change: function(event, {value}) {
-      const valueNode = $this?.prev()?.children()?.last('.calc-label__summ');
-      if (valueNode) {
-        const newValue = value >= 10000 ? `${value / 1000} 000`  : value;
-        if (separator !== '₽') {
-          separator = getDataSeparator(value);
-        }
+$(document).ready(function() {
+  initRangeSliders();
+  initSlick();
+});
 
-        const strVal = `${newValue} ${separator}`;
-        valueNode.text(strVal);
-        separator === '₽' ? $summ.text(strVal) : setCalcDate(value);
+function initSlick() {
+  $('.carousel').slick({
+    dots: true,
+    infinite: true,
+    speed: 300,
+  });
+}
+
+function initRangeSliders() {
+  $date = $('.calc-summary__item--date .calc-summary__value');
+  $summ = $('.calc-summary__item--summ .calc-summary__value');
+  $('.range-slider').each(function() {
+    var min = Number($(this).attr('data-min')),
+        max = Number($(this).attr('data-max')),
+        value = Number($(this).attr('data-value')),
+        step = Number($(this).attr('data-step')),
+        separator = $(this).attr('data-separator'),
+        $this = $(this);
+        if (separator !== '₽') setCalcDate(value);
+    
+    $this.slider({
+      range: 'min',
+      value,
+      min,
+      max,
+      step,
+      change: function(event, {value}) {
+        const valueNode = $this?.prev()?.children()?.last('.calc-label__summ');
+        if (valueNode) {
+          const newValue = value >= 10000 ? `${value / 1000} 000`  : value;
+          if (separator !== '₽') {
+            separator = getDataSeparator(value);
+          }
+
+          const strVal = `${newValue} ${separator}`;
+          valueNode.text(strVal);
+          separator === '₽' ? $summ.text(strVal) : setCalcDate(value);
+        }
       }
-    }
+    });
+
+    $this.find('.value').html(value);
   });
 
-  $this.find('.value').html(value);
-});
-
-$('.range-slider-value').on('touch click', function() {
-  $(this)?.parent()?.prev()?.slider(
-    'value',
-    Number($(this).text().split(' ').slice(0, -1).join(''))
-  );
-});
+  $('.range-slider-value').on('touch click', function() {
+    $(this)?.parent()?.prev()?.slider(
+      'value',
+      Number($(this).text().split(' ').slice(0, -1).join(''))
+    );
+  });
+}
 
 function getDataSeparator(value) {
   switch (value) {
